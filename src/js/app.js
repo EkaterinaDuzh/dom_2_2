69bd7e258d8df5ac95b2c28aa7dfa792cc0f07e4
Ekaterina <ekaterinaduzh@gmail.com>
2025-05-12 16:58:29 +0300

    Добавление тестов

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
-import Field from './Field';
-import GamePlay from './GamePlay';
+import createCells from './cells';
+import setSize from './setSize';
+import definePosition from './definePosition';
+import timerClock from './timer';
 
-const fields = new Field();
-const gameplay = new GamePlay(fields);
-gameplay.init();
+const size = setSize();
+const field = createCells(size);
+const body = document.querySelector('body');
+body.append(field);
+field.style.setProperty('--side', size);
+definePosition();
+timerClock();
